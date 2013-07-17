cb441374a1d260af49661c397f46a9c4853397e2
Julius Seltenheim <mail@julius-seltenheim.com>
2013-07-17 19:51:00 +0200

    sh to generate timeline.js

---
+++ createSingleJs.sh
@@ -0,0 +1,11 @@
+echo "creating timeline.js..."
+
+cd js
+rm timeline.js
+
+cat "classes\timeline.js" > timeline.js
+cat "classes\timelineEntry.js" >> timeline.js
+cat "classes\tooltip.js" >> timeline.js
+
+cd ..
+echo "creation successful"
