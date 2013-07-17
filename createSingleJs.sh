e311fa73f08cdb4966b24ab541eeae4326ac8701
Julius Seltenheim <mail@julius-seltenheim.com>
2013-07-17 19:54:20 +0200

    generate.sh fix

--- createSingleJs.sh
+++ createSingleJs.sh
@@ -3,9 +3,9 @@ echo "creating timeline.js..."
 cd js
 rm timeline.js
 
-cat "classes\timeline.js" > timeline.js
-cat "classes\timelineEntry.js" >> timeline.js
-cat "classes\tooltip.js" >> timeline.js
+cat "classes/timeline.js" > timeline.js
+cat "classes/timelineEntry.js" >> timeline.js
+cat "classes/tooltip.js" >> timeline.js
 
 cd ..
 echo "creation successful"
