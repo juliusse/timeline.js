0617b6dc977bec25622cecf6c583bbde2e659a40
Julius Seltenheim <mail@julius-seltenheim.com>
2013-07-24 01:46:16 +0200

    added google closure compiler

--- createSingleJs.bat
+++ createSingleJs.bat
@@ -1,11 +1,18 @@
 echo "creating timeline.js..."
 
 cd js
+rm timeline_big.js
+rm timeline_compiled.js
 rm timeline.js
 
-echo  /**/ > timeline.js
-for /r %%i in (*) do type %%i >> timeline.js
+touch timeline_big.js
+for /r %%i in (*) do type %%i >> timeline_big.js
+echo. >> timeline_big.js
 
+java -jar ../compiler.jar --js timeline_big.js --js_output_file timeline_compiled.js
+
+echo /* author: Julius Seltenheim (mail@julius-seltenheim.com) */ > timeline.js
+type timeline_compiled.js >> timeline.js
 
 cd ..
 echo "creation successful"
