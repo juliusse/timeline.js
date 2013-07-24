55812809c2f8c6cc07be470da05c860f5872a625
Julius Seltenheim <mail@julius-seltenheim.com>
2013-07-24 09:39:19 +0200

    generate js in bin folder

--- createSingleJs.bat
+++ createSingleJs.bat
@@ -1,18 +1,21 @@
 echo "creating timeline.js..."
 
-cd js
-rm timeline_big.js
-rm timeline_compiled.js
-rm timeline.js
+mkdir bin
+mkdir bin\js
 
+rm js\timeline_big.js
+rm js\timeline_compiled.js
+rm bin\js\timeline.js
+
+cd js
 touch timeline_big.js
 for /r %%i in (*) do type %%i >> timeline_big.js
 echo. >> timeline_big.js
 
-java -jar ../compiler.jar --js timeline_big.js --js_output_file timeline_compiled.js
+java -jar ..\compiler.jar --js timeline_big.js --js_output_file timeline_compiled.js
 
-echo /* author: Julius Seltenheim (mail@julius-seltenheim.com) */ > timeline.js
-type timeline_compiled.js >> timeline.js
+echo /* author: Julius Seltenheim (mail@julius-seltenheim.com) */ > ..\bin\js\timeline.js
+type timeline_compiled.js >> ..\bin\js\timeline.js
 
 cd ..
 echo "creation successful"
