a1490391d776bf18b45452ba5f5df8ce55b13194
Julius Seltenheim <julius.seltenheim@gmail.com>
2019-01-14 01:45:16 -0300

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,11 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: {
-        timeline: './js/index.js',
-        exampleApp: './js/exampleApp.js'
-    },
+    entry: './js/index.js',
     output: {
-        filename: '[name].js',
-        path: path.join(__dirname, 'bin')
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'bin'),
-        compress: true,
-        port: 9000
+        filename: 'timeline.js',
+        path: path.join(__dirname, 'bin'),
+        library: 'TimelineJs',
+        libraryTarget:'window'
     }
 };
